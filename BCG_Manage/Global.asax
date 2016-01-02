d820ae03f94133f9bf87639fedf9da97a1dd06c2
baiganio <big_l@abv.bg>
2016-01-02 12:26:22 +0200

    added on 2/1/2016

---
+++ BCG_Manage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BCG_Manage.MvcApplication" Language="C#" %>
---
+++ BCG_Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BCG_Portal.Startup))]
+namespace BCG_Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
