88b8046d8bbf6ceb88415c169da5c6376ef0d6aa
Svetlin <svetlin.krastanov90@gmail.com>
2016-02-07 20:18:56 +0200

    Added Discounts, some controllers TODO: products and shoppers, check shopper groups and coupons for shoppers/groups

--- BCG_Manage/Areas/Store/Controllers/DiscountsController.cs
+++ BCG_Manage/Areas/Store/Controllers/DiscountsController.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace BCG_Manage.Areas.Store.Controllers
+﻿namespace BCG_Manage.Areas.Store.Controllers
 {
-    public class DiscountsController : Controller
+    using BCG_DB.Entity.Store;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using PagedList;
+    using System.IO;
+    using System.Drawing;
+    using System.Data.Entity;
+    using Models;
+    using System.Configuration;
+    using System.Data.SqlClient;
+    using BCG_Manage.Controllers;
+
+    public class DiscountsController : BaseController
     {
         // GET: Store/Discounts
-        public ActionResult Index()
+        public ActionResult AddNewDiscount()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAllDiscounts()
+        {
+            return View();
+        }
+
+        public ActionResult ApplyDiscountToUsers()
+        {
+            return View();
+        }
+
+        public ActionResult ApplyDiscountToProducts()
         {
             return View();
         }
