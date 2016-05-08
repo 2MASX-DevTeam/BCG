25d1c93cf545bb05fa2c7042c73d4b40954cdb9f
Republic Of Gamers <Republic Of Gamers@DESKTOP-QGEKTLN>
2016-05-08 16:13:01 +0300

    change few things when send e-mails

--- BCG-master/BCG_Portal/Helpers/SendMailHelper.cs
+++ BCG-master/BCG_Portal/Helpers/SendMailHelper.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 using System.Web;
@@ -17,65 +18,64 @@ namespace BCG_Portal.Helpers
         private string credentialsPassword = ConfigurationManager.AppSettings["credentialsPassword"];
         private string emailCredentials = ConfigurationManager.AppSettings["emailCredentials"];
         private string appName = ConfigurationManager.AppSettings["ApplicationName"];
-        private SmtpClient client = new SmtpClient();
+        private SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
         private MailMessage mail = new MailMessage();
 
         /// <summary>
         /// Use this to inform the admins for any issues
         /// </summary>
         /// <param name="message">Represents the error which is throwen by the application</param>
-        public void SendExceptionToAdmin(string message)
-        {
-            client.Credentials = new System.Net.NetworkCredential(emailCredentials, credentialsPassword);
-            client.Port = 587; // Gmail works on this port
-            client.Host = "smtp.gmail.com";
-            client.EnableSsl = true; //Gmail works on Server Secured Layer
+        //public void SendExceptionToAdmin(string message)
+        //{
+        //    client.Credentials = new System.Net.NetworkCredential(emailCredentials, credentialsPassword);
+        //    client.Port = 587; // Gmail works on this port
+        //    client.Host = "smtp.gmail.com";
+        //    client.EnableSsl = true; //Gmail works on Server Secured Layer
 
-            string app = string.Format("{0} Exception Handler", ConfigurationManager.AppSettings["ApplicationName"]);
-            mail.To.Add(emailTo);
-            mail.From = new MailAddress(emailFrom, app, System.Text.Encoding.UTF8);
-            mail.Subject = string.Format("{0} APP Throw an exception!.", ConfigurationManager.AppSettings["ApplicationName"]);
-            mail.SubjectEncoding = System.Text.Encoding.UTF8;
-            mail.Body = message;
-            mail.BodyEncoding = System.Text.Encoding.UTF8;
-            mail.IsBodyHtml = true;
-            mail.Priority = MailPriority.High;
+        //    string app = string.Format("{0} Exception Handler", ConfigurationManager.AppSettings["ApplicationName"]);
+        //    mail.To.Add(emailTo);
+        //    mail.From = new MailAddress(emailFrom, app, System.Text.Encoding.UTF8);
+        //    mail.Subject = string.Format("{0} APP Throw an exception!.", ConfigurationManager.AppSettings["ApplicationName"]);
+        //    mail.SubjectEncoding = System.Text.Encoding.UTF8;
+        //    mail.Body = message;
+        //    mail.BodyEncoding = System.Text.Encoding.UTF8;
+        //    mail.IsBodyHtml = true;
+        //    mail.Priority = MailPriority.High;
 
-            TryToSendMail();
-        }
+        //    TryToSendMail();
+        //}
 
         /// <summary>
         /// Use this to inform the admins for any issues
         /// </summary>
         /// <param name="appName">Current application name. Usefull to recognize from wich application the error is thrown</param>
-        public void SendExceptionToAdmin(string message, string appName)
-        {
-            client.Credentials = new System.Net.NetworkCredential(emailFrom, credentialsPassword);
-            client.Port = 587; // Gmail works on this port
-            client.Host = "smtp.gmail.com";
-            client.EnableSsl = true; //Gmail works on Server Secured Layer
+        //public void SendExceptionToAdmin(string message, string appName)
+        //{
+        //    client.Credentials = new System.Net.NetworkCredential(emailFrom, credentialsPassword);
+        //    client.Port = 587; // Gmail works on this port
+        //    client.Host = "smtp.gmail.com";
+        //    client.EnableSsl = true; //Gmail works on Server Secured Layer
 
-            mail.To.Add(emailTo);
-            string info = string.Format("{0} Exception Handler", appName);
-            mail.From = new MailAddress(emailFrom, info, System.Text.Encoding.UTF8);
-            string subject = string.Format("{0} Throw an exception!.", appName);
-            mail.Subject = subject;
-            mail.SubjectEncoding = System.Text.Encoding.UTF8;
-            mail.Body = message;
-            mail.BodyEncoding = System.Text.Encoding.UTF8;
-            mail.IsBodyHtml = true;
-            mail.Priority = MailPriority.High;
+        //    mail.To.Add(emailTo);
+        //    string info = string.Format("{0} Exception Handler", appName);
+        //    mail.From = new MailAddress(emailFrom, info, System.Text.Encoding.UTF8);
+        //    string subject = string.Format("{0} Throw an exception!.", appName);
+        //    mail.Subject = subject;
+        //    mail.SubjectEncoding = System.Text.Encoding.UTF8;
+        //    mail.Body = message;
+        //    mail.BodyEncoding = System.Text.Encoding.UTF8;
+        //    mail.IsBodyHtml = true;
+        //    mail.Priority = MailPriority.High;
 
-            TryToSendMail();
-        }
+        //    TryToSendMail();
+        //}
 
         internal Task SendEmailConfirmation(IdentityMessage message)
         {
-
-            client.Credentials = new System.Net.NetworkCredential(emailCredentials, credentialsPassword);
-            client.Port = 587; // Gmail works on this port
-            client.Host = "smtp.gmail.com";
-            client.EnableSsl = true; //Gmail works on Server Secured Layer
+            //client.Credentials = new NetworkCredential(emailCredentials, credentialsPassword);
+            //client.Port = 587; // Gmail works on this port
+            //client.Host = "smtp.gmail.com";
+           // client.EnableSsl = true; //Gmail works on Server Secured Layer
 
             string app = string.Format("{0} Registration request", appName);
             mail.To.Add(message.Destination.ToString());
@@ -106,7 +106,7 @@ namespace BCG_Portal.Helpers
                     ex2 = ex2.InnerException;
                 }
 
-                SendExceptionToAdmin(errorMessage, "TryToSendMail()");
+               // SendExceptionToAdmin(errorMessage, "TryToSendMail()");
             }
         }
 
